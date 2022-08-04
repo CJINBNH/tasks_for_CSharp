b842e57918828346eb13fa0b0dc5aae78e4f9260
CJINBNH <alex_egorov_89@mail.ru>
2022-08-04 22:44:02 +0400

    added task2

---
+++ task2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine ("Введите первое число: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите второе число: ");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+if (numberTwo > numberOne)
+{
+    Console.WriteLine(numberTwo / numberOne);
+}
+else
+{
+    Console.WriteLine(numberOne / numberTwo);
+}
