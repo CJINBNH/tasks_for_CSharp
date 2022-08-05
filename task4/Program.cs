34912af92b299c4f0442ba4ede0a256bd1677d3f
CJINBNH <alex_egorov_89@mail.ru>
2022-08-05 21:32:05 +0400

    Completed task4 with while cycle

---
+++ task4/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+int count = -numberN;
+if(numberN > 0)
+{
+    while (count <= numberN)
+    {
+        Console.Write(count);
+        count = count + 1;
+    }
+}
+else
+{
+    while (count >= numberN)
+    {
+        Console.Write(count);
+        count = count - 1;
+    }
+}
