8d2ce103b276993ceb27e260067655fe47f28c78
CJINBNH <alex_egorov_89@mail.ru>
2022-08-05 21:48:51 +0400

    Comleted task5 with if/else construction

--- task5/Program.cs
+++ task5/Program.cs
@@ -2,3 +2,12 @@
 Console.WriteLine("Введите число: ");
 int N = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine(N%10);
+//ограничиваем вводимое значение с помощью конструкции if/else
+if(N > 99 & N < 1000)
+{
+    Console.WriteLine(N%10);
+}
+else
+{
+    Console.WriteLine("Число не подходит");
+}
