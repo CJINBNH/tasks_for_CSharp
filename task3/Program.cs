ad152278d4b7d84382f407bad973bfe6af0afe18
CJINBNH <alex_egorov_89@mail.ru>
2022-08-05 21:13:00 +0400

    Comleted task3 with switch

---
+++ task3/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+Console.WriteLine("Введите день недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+//решение с помощью switch
+switch(day)
+{
+    case 1:
+        Console.WriteLine("Sunday");
+        break;
+    case 2:
+        Console.WriteLine("Monday");
+        break;
+    case 3:
+        Console.WriteLine("Tuesday");
+        break;
+    case 4:
+        Console.WriteLine("Wednesday");
+        break;
+    case 5:
+        Console.WriteLine("Thursday");
+        break;
+    case 6:
+        Console.WriteLine("Friday");
+        break;
+    case 7:
+        Console.WriteLine("Saturday");
+        break;
+    default:
+        Console.WriteLine("This is not a week or weekend day");
+        break;
+}
