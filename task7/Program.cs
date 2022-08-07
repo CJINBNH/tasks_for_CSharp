3d2107b160f75d63f7fa86aca6e89dbbafa49306
CJINBNH <alex_egorov_89@mail.ru>
2022-08-07 20:28:23 +0400

    Completed home task7

---
+++ task7/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int thirdNumber = Convert.ToInt32(Console.ReadLine());
+int max = firstNumber;
+//нужно сравнить два числа и вывести большее
+if (firstNumber > secondNumber)
+{
+    Console.Write("max = ");
+}
+else if (secondNumber > thirdNumber)
+{
+    max = secondNumber;
+    Console.Write("max = ");
+}
+else if (thirdNumber > firstNumber)
+{
+    max = thirdNumber;
+    Console.Write("max = ");
+}
+Console.WriteLine(max);
