3f591920da2dc13a0bd42d47a609a376976923e4
CJINBNH <alex_egorov_89@mail.ru>
2022-08-07 21:04:28 +0400

    completed home task8

---
+++ task8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите новое число: ");
+int evenOdd = Convert.ToInt32(Console.ReadLine());
+if (evenOdd % 2 != 0)
+{
+    Console.WriteLine("Это число нечетное");
+}
+else
+{
+    Console.WriteLine("Это число четное");
+}
