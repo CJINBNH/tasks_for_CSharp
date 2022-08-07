b6d605cf47a75a62c5055cf46818b39733e9c171
CJINBNH <alex_egorov_89@mail.ru>
2022-08-07 21:40:22 +0400

    Completed home task9

---
+++ task9/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.WriteLine("Введите любое целое число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while (count <= N)
+{
+    if (count % 2 != 0)
+    {
+        count = count + 1;
+        Console.Write(count);
+    }
+    else
+    {
+        count = count +1;
+    }
+}
